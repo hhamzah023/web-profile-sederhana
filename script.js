5ba09091a6fc9b34d9ae97cf2e379f223fd31d7d
hhamzah023 <hajrilhamzah023@gmail.com>
2026-01-30 23:46:40 +0700

    update website

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Website profil berhasil dijalankan!");
+const images = document.querySelectorAll(".portfolio-img");
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close");
+
+images.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "block";
+    modalImg.src = img.src;
+  });
+});
+
+closeBtn.onclick = () => {
+  modal.style.display = "none";
+};
+
+modal.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+};
